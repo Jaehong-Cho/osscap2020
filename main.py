c622342e299afc028a34ef430c8122ce63c004e3
Hyungchan <miggari123@naver.com>
2020-11-25 13:46:24 +0900

    merge

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-from realdino import *
+from py_realdino import *
 import requests
 from bs4 import BeautifulSoup
 import time
 import schedule
 from auto_updating_naverWeather import *
+import threading
 
-schedule.every(1).minutes.do(get_info)
+def weather_init ():
+    w_thread = threading.Thread(target=weather_main, args=())
+    w_thread.setDaemon(True)
+    w_thread.start()
+    return
 
-while True:
-	
-	#auto_weather
-	schedule.run_pending()
-	time.sleep(1)
+weather_init()
+
+while True:  
 	
     print("MODE")
     print("1. dino game \n2.Turn off")
     choice = int(input("Select the Mode : "))
     if choice == 1:
-        realdino()
+        py_realdino()
         continue
 
     elif choice == 2:
