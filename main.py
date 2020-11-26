839b94bf171801daf0d4355aeac3351548b6d01d
ryusangbum <70688711+ryusangbum@users.noreply.github.com>
2020-11-26 19:08:51 +0900

    Update main.py
    추가기능 구현

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from py_realdino import *
+from assistant_info import *
+from first_weather import *
+from second_weather import *
+import time
 import requests
 from bs4 import BeautifulSoup
 import time
@@ -6,26 +10,34 @@ import schedule
 from auto_updating_naverWeather import *
 import threading
 
-def weather_init ():
+
+def weather_init():
     w_thread = threading.Thread(target=weather_main, args=())
     w_thread.setDaemon(True)
     w_thread.start()
     return
 
+
 weather_init()
 
-while True:  
-	
+assistant_info()
+while True:
+
     print("MODE")
-    print("1. dino game \n2.Turn off")
+    print("1.dino game \n2.weather info \n3.Turn off")
     choice = int(input("Select the Mode : "))
     if choice == 1:
         py_realdino()
         continue
-
     elif choice == 2:
-        continue
-        
+        print()
+    elif choice == 3:
+        break
     else:
         print("잘못된 번호를 입력하셨습니다.")
         continue
+
+    first_weather()
+    time.sleep(5)
+    second_weather()
+    time.sleep(5)
